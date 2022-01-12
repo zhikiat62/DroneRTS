3b2370822c4e1ad192fbc0875124f2db3eb9208a
zhikiat62 <64786627+zhikiat62@users.noreply.github.com>
2022-01-12 13:59:57 +0800

    1. Requirements file

---
+++ face-detect-drone.py
@@ -0,0 +1,177 @@
+# simple example demonstrating how to control a Tello using your keyboard.
+# For a more fully featured example see manual-control-pygame.py
+# 
+# Use W, A, S, D for moving, E, Q for rotating and R, F for going up and down.
+# When starting the script the Tello will takeoff, pressing ESC makes it land
+#  and the script exit.
+
+# 简单的演示如何用键盘控制Tello
+# 欲使用全手动控制请查看 manual-control-pygame.py
+#
+# W, A, S, D 移动， E, Q 转向，R、F上升与下降.
+# 开始运行程序时Tello会自动起飞，按ESC键降落
+# 并且程序会退出
+
+from djitellopy import Tello
+import cv2, math, time
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet, args):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+        (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > args["confidence"]:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            if face is not None:
+                # face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+
+                # add the face and bounding boxes to their respective
+                # lists
+                faces.append(face)
+                locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=35)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
+tello = Tello()
+tello.connect()
+
+tello.takeoff()
+# initialize the video stream and allow the camera sensor to warm up
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", type=str,
+    default="face_detect_model",
+    help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+    default="mask_detect_model",
+    help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized face detector model from disk
+print("[INFO] loading face detector model...")
+prototxtPath = os.path.join(os.getcwd(), args["face"] , "deploy.prototxt") 
+weightsPath = os.path.join(os.getcwd(), args["face"] ,"res10_300x300_ssd_iter_140000.caffemodel")
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+maskModelPath = os.path.join(os.getcwd(), args["model"] , "mask-detector-model.model")
+maskNet = load_model(maskModelPath)
+
+
+while True:
+    # In reality you want to display frames in a seperate thread. Otherwise
+    #  they will freeze while the drone moves.
+    # 在实际开发里请在另一个线程中显示摄像头画面，否则画面会在无人机移动时静止
+    print(tello.get_battery())
+    img = frame_read.frame
+    img = imutils.resize(img, width=400)
+
+    # detect faces in the frame and determine if they are wearing a
+    # face mask or not
+    (locs, preds) = detect_and_predict_mask(img, faceNet, maskNet, args)
+
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(img, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(img, (startX, startY), (endX, endY), color, 2)
+
+    # show the output frame
+    cv2.imshow("drone", img)
+
+    key = cv2.waitKey(1) & 0xff
+    if key == 27:  # ESC
+        break
+    elif key == ord('w'):
+        tello.move_forward(30)
+    elif key == ord('s'):
+        tello.move_back(30)
+    elif key == ord('a'):
+        tello.move_left(30)
+    elif key == ord('d'):
+        tello.move_right(30)
+    elif key == ord('e'):
+        tello.rotate_clockwise(30)
+    elif key == ord('q'):
+        tello.rotate_counter_clockwise(30)
+    elif key == ord('r'):
+        tello.move_up(30)
+    elif key == ord('f'):
+        tello.move_down(30)
+
+tello.land();
+cv2.destroyAllWindows()
